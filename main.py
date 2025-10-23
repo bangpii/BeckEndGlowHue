dd29ef06bd9d0aa0c983ba68d6458395333d8019
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-10-23 10:55:26 +0700

    Prepare backend for Railway deployment

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.register_blueprint(foto_bp, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
